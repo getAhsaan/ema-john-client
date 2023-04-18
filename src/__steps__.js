24da3f6561686f8f99414a0c015c0912c506416f
getahsaan <getahsaan@gamil.com>
2023-04-18 17:45:11 +0600

    firebase auth project initialize

---
+++ src/__steps__.js
@@ -0,0 +1,7 @@
+/**
+ * create firebase project
+ * create web app
+ * npm install firebase
+ * build>authentication>get started>enable signin method
+ * create signup and login
+ * **/
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBeihnUog5BautHBF9nyr6yaJZHOEOCbok",
+  authDomain: "ema-john-with-firebase-a-c7908.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a-c7908",
+  storageBucket: "ema-john-with-firebase-a-c7908.appspot.com",
+  messagingSenderId: "960765311181",
+  appId: "1:960765311181:web:92a478b0681dfdaf641184",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
